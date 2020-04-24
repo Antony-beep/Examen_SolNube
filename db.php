96d87e05b8b73c8a0e141a67f126b4444a4424e4
Antony-beep <54334368+Antony-beep@users.noreply.github.com>
2020-04-23 19:46:13 -0500

    Update db.php

--- db.php
+++ db.php
@@ -2,10 +2,10 @@
 session_start();
 
 $conn = mysqli_connect(
-  'localhost',
-  'root',
-  '',
-  'php_mysql_crud'
+  'us-cdbr-iron-east-01.cleardb.net',
+  'b76c462e2a2eba',
+  '7220f035',
+  'heroku_7aa9b5beaf04b32'
 ) or die(mysqli_erro($mysqli));
 
 ?>
